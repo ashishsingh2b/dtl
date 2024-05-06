3107b524955a4f01c2d561457d1d6acfcf0f7595
Ashish Singh <ashishsingh2b@gmail.com>
2024-05-06 12:32:39 +0530

    second commit

--- mytemp/app/views.py
+++ mytemp/app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def home(request):
-    name= "Ashis Singh"
-    age= 26
-    mobileno=8487840633
-    mydata = {'nm': name,'a':age,'mo':mobileno}
-    return render(request,'home.html',mydata)
+        students = {'names':['Ashish','Rahul','Rajan','virendra']}
+        return render(request,'home.html',students)
+    
+    
